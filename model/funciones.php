946e7f87f371d413b3ab24a766a13e5d47613e0c
damnedDeamon <33160500+damnedDeamon@users.noreply.github.com>
2024-08-23 12:29:41 -0400

    actualizacion de vistas
    se modifico el ccs de la vista de mascotas, antiparasitarios y vacunas

--- model/funciones.php
+++ model/funciones.php
@@ -5,11 +5,17 @@ if($accion=="mostrarMascota"){
     <div class="container2">
         <div class="header">
             <a href="javascript:void(0);" onclick="volverMenu()"><img src="../img/volver.png" width="50" height="50" /></a><span class="title">Detalle Mascota</span>
-            <span class="date" >Fecha: <span id="Fecha"></span></span>
+            <span class="date" > <span id="Fecha"></span></span>
         </div>
         <div class="content">
             <div class="sidebar">
-                <!-- Aquí podrías agregar una imagen o información adicional -->
+                <div class="item">Bloque 1</div>
+                <div class="item">Bloque 2</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <!-- Más bloques pueden ser agregados aquí -->
             </div>
             <div class="main-content">
                 <div class="info-box"></div>
@@ -17,6 +23,7 @@ if($accion=="mostrarMascota"){
         </div>
     </div>
     <script>
+        /*
         var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -25,10 +32,10 @@ if($accion=="mostrarMascota"){
         today = dd + '/' + mm + '/' + yyyy;
         console.log(today);
         document.getElementById("Fecha").innerHTML = today;
+        */
     </script>
     <?php	
 }
-
 
 if($accion=="mostrarAnti"){
 	?>
@@ -36,20 +43,17 @@ if($accion=="mostrarAnti"){
     <div class="container2">
         <div class="header">
             <a href="javascript:void(0);" onclick="volverMenu2()"><img src="../img/volver.png" width="50" height="50" /></a><span class="title">Detalle Antiparasitario</span>
-            <span class="date" >Fecha: <span id="Fecha"></span></span>
+            <span class="date" ><span id="Fecha"></span></span>
         </div>
         <div class="content">
             <div class="sidebar">
-                <!-- Aquí podrías agregar una imagen o información adicional -->
-            </div>
-            <div class="sidebar">
-                <!-- Aquí podrías agregar una imagen o información adicional -->
-            </div>
-            <div class="sidebar">
-                <!-- Aquí podrías agregar una imagen o información adicional -->
-            </div>
-            <div class="sidebar">
-                <!-- Aquí podrías agregar una imagen o información adicional -->
+                <div class="item">Bloque 1</div>
+                <div class="item">Bloque 2</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <!-- Más bloques pueden ser agregados aquí -->
             </div>
             <div class="main-content">
                 <div class="info-box"></div>
@@ -57,6 +61,7 @@ if($accion=="mostrarAnti"){
         </div>
     </div>
     <script>
+        /* 
         var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -65,6 +70,7 @@ if($accion=="mostrarAnti"){
         today = dd + '/' + mm + '/' + yyyy;
         console.log(today);
         document.getElementById("Fecha").innerHTML = today;
+        */
     </script>
     <?php	
 }
@@ -75,11 +81,17 @@ if($accion=="mostrarVacuna"){
     <div class="container2">
         <div class="header">
             <a href="javascript:void(0);" onclick="volverMenu3()"><img src="../img/volver.png" width="50" height="50" /></a><span class="title">Detalle Vacuna</span>
-            <span class="date" >Fecha: <span id="Fecha"></span></span>
+            <span class="date" ><span id="Fecha"></span></span>
         </div>
         <div class="content">
             <div class="sidebar">
-                <!-- Aquí podrías agregar una imagen o información adicional -->
+                <div class="item">Bloque 1</div>
+                <div class="item">Bloque 2</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <div class="item">Bloque 3</div>
+                <!-- Más bloques pueden ser agregados aquí -->
             </div>
             <div class="main-content">
                 <div class="info-box"></div>
@@ -87,6 +99,7 @@ if($accion=="mostrarVacuna"){
         </div>
     </div>
     <script>
+        /*
         var today = new Date();
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -95,6 +108,7 @@ if($accion=="mostrarVacuna"){
         today = dd + '/' + mm + '/' + yyyy;
         console.log(today);
         document.getElementById("Fecha").innerHTML = today;
+         */
     </script>
     <?php	
 }
